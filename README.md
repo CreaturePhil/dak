90c52a1b4183a22362528a0d777e8fc509ed4366
Phil <birkal@outlook.com>
2017-02-27 17:13:08 -0800

    feat(project): Initial release

---
+++ README.md
@@ -0,0 +1,27 @@
+# dak
+
+(Work in Progress)
+
+Static type checker with [Hindley–Milner type system](https://en.wikipedia.org/wiki/Hindley%E2%80%93Milner_type_system) syntax
+
+## Install
+
+```
+$ npm install --global dak
+```
+
+## Usage
+
+```
+$ dak --help
+
+Usage
+  $ dak <file>
+
+  Examples
+    $ dak file.js
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ file.js
@@ -0,0 +1,33 @@
+// @dak
+
+/* hi */
+
+
+/*
+@type str :: Number
+*/
+var str = 'hello world!';
+console.log(str);
+
+
+// @type foo :: Number -> Number
+function foo(x) {
+  return x * 10;
+}
+
+foo('Hello, world!');
+foo(10);
+
+// @type foo2 :: String -> Number -> String
+function foo2(x, y) {
+  return x.length * y;
+}
+
+foo2('Hello', 42);
+
+// @type strLength :: String -> Number
+var strLength = function(s) {
+  return s.length;
+}
+
+strLength({});
