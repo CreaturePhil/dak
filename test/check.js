f8055f9f6f0cab1a9eb07a4b8c70b458495ddaf0
Phil <birkal@outlook.com>
2017-03-01 19:44:09 -0800

    test(check): Function arguments and return type

--- test/check.js
+++ test/check.js
@@ -52,3 +52,78 @@ test('type mismatch variable and correct type', t => {
   ];
   t.deepEqual(check(contents, typeDefs), expectedResult);
 });
+
+test('type mismatch function argument', t => {
+  const contents = `
+  // @type foo :: Number -> Number
+
+  function foo(x) {
+    return x * 10;
+  }
+
+  foo('Hello, world!');
+  `;
+  const typeDefs = [{
+    comment: '// @type foo :: Number -> Number',
+    name: 'foo',
+    types: ['Number', 'Number']
+  }];
+  const expectedResult = [
+    {
+      check: 'error',
+      comment: '// @type foo :: Number -> Number',
+      name: 'foo',
+      actual: 'String',
+      expected: 'Number',
+      line: "foo('Hello, world!');",
+      lineNumber: 7,
+      isArg: true
+    },
+    {
+      check: 'success',
+      name: 'foo',
+      returnValue: NaN
+    }
+  ];
+  t.deepEqual(check(contents, typeDefs), expectedResult);
+});
+
+test('type mismatch function argument and return type', t => {
+  const contents = `
+  // @type strLength :: String -> Number
+
+  var strLength = function(s) {
+    return s.length;
+  }
+
+  strLength({});
+  `;
+  const typeDefs = [{
+    comment: '// @type strLength :: String -> Number',
+    name: 'strLength',
+    types: ['String', 'Number']
+  }];
+  const expectedResult = [
+    {
+      check: 'error',
+      comment: '// @type strLength :: String -> Number',
+      name: 'strLength',
+      actual: 'Object',
+      expected: 'String',
+      line: 'strLength({});',
+      lineNumber: 7,
+      isArg: true
+    },
+    {
+      check: 'error',
+      comment: '// @type strLength :: String -> Number',
+      name: 'strLength',
+      actual: 'Undefined',
+      expected: 'Number',
+      line: 'strLength({});',
+      lineNumber: 7,
+      isReturn: true
+    }
+  ];
+  t.deepEqual(check(contents, typeDefs), expectedResult);
+});
